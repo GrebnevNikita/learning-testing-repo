50c62c39ff79f09a4bacc2b654e31637f59cf8f1
fuble <futer25@mail.ru>
2024-04-01 22:05:35 +0300

    php

--- PHP/BASIC/13_if_else_case_for.php
+++ PHP/BASIC/13_if_else_case_for.php
@@ -11,5 +11,39 @@ if ($a > $b) {
 for ($i = 1; $i <= 10; $i++) {
     echo $i;
 }
+
+
+$arr = array(1, 2, 3, 4);
+foreach ($arr as &$value) {
+    $value = $value * 2;
+}
+
+
+switch ($i) {
+    case 0:
+        echo "i equals 0";
+        break;
+    case 1:
+        echo "i equals 1";
+        break;
+    case 2:
+        echo "i equals 2";
+        break;
+}
+
+
+//match usage
+$food = 'cake';
+
+$return_value = match ($food) {
+    'apple' => 'This food is an apple',
+    'bar' => 'This food is a bar',
+    'cake' => 'This food is a cake',
+};
+
+//string(19) "This food is a cake"
+
+
+
 ?>
 
