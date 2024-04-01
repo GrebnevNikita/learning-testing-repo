108c32755bc9a725ef9952c873a47033098f2d7f
fuble <futer25@mail.ru>
2024-04-01 23:54:33 +0300

    php

---
+++ PHP/BASIC/17_function_1.php
@@ -0,0 +1,50 @@
+Variable function example
+
+<?php
+function foo() {
+    echo "In foo()<br />\n";
+}
+
+function bar($arg = '')
+{
+    echo "In bar(); argument was '$arg'.<br />\n";
+}
+
+// This is a wrapper function around echo
+function echoit($string)
+{
+    echo $string;
+}
+
+$func = 'foo';
+$func();        // This calls foo()
+
+$func = 'bar';
+$func('test');  // This calls bar()
+
+$func = 'echoit';
+$func('test');  // This calls echoit()
+?>
+
+Variable method example
+
+<?php
+class Foo
+{
+    function Variable()
+    {
+        $name = 'Bar';
+        $this->$name(); // This calls the Bar() method
+    }
+
+    function Bar()
+    {
+        echo "This is Bar";
+    }
+}
+
+$foo = new Foo();
+$funcname = "Variable";
+$foo->$funcname();  // This calls $foo->Variable()
+
+?>
