66e5cdd8d4ab024401505fab854ed970b0888eff
fuble <futer25@mail.ru>
2024-04-01 14:01:58 +0300

    php

---
+++ PHP/BASIC/basic_2.php
@@ -0,0 +1,42 @@
+Every single expression in PHP has one of the following built-in types depending on its value:
+
+null
+bool
+int
+float (floating-point number)
+string
+array
+object
+callable
+resource
+
+
+
+Atomic types ¶
+Some atomic types are built-in types which are tightly integrated with the language and cannot be reproduced with user defined types.
+
+The list of base types is:
+    Built-in types
+        null type
+        Scalar types:
+            bool type
+            int type
+            float type
+            string type
+            array type
+            object type
+            resource type
+            never type
+            void type
+            Relative class types: self, parent, and static
+    Value types
+        false
+        true
+    User-defined types (generally referred to as class-types)
+        Interfaces
+        Classes
+        Enumerations
+    callable type
+
+
+
